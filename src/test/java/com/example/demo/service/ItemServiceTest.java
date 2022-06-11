4ebf95c225015fe26c93d15a0032f769f7d14e15
ayn030 <kojinco18.033@gmail.com>
2022-06-11 11:40:00 +0900

    メソッド名を日本語にしてDisplayNameを削除

--- src/test/java/com/example/demo/service/ItemServiceTest.java
+++ src/test/java/com/example/demo/service/ItemServiceTest.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,8 +38,7 @@ public class ItemServiceTest {
 	}
 
 	@Test
-	@DisplayName("検索idに1を渡すとid=１に基づいたデータと一致する")
-	public void testFindById() {
+	public void 検索idに1を渡すとidが１に紐づいたデータと一致する() {
 		int id = 1;
 		when(itemMapper.findById(id)).thenReturn(Optional.of(item));
 
@@ -53,16 +51,14 @@ public class ItemServiceTest {
 	}
 
 	@Test
-	@DisplayName("検索idにNULLを渡してもNullPointerExceptionにならない")
-	public void testFindByIdNotNull() {
+	public void 検索idにNULLを渡してもNullPointerExceptionにならない() {
 		int id = 1;
 		when(itemMapper.findById(id)).thenReturn(Optional.of(item));
 		assertNotNull(itemService.findById(2));
 	}
 
 	@Test
-	@DisplayName("データをリスト表示するとデータ数が一致する")
-	public void testGetItemList() {
+	public void データをリスト表示するとデータ数が一致する() {
 		Item item1 = new Item(2, "マーガレットピアス", 2400, "ピアス", 3);
 		Item item2 = new Item(3, "チューリップイヤリング", 3450, "イヤリング", 5);
 		when(itemMapper.findAll()).thenReturn(List.of(item1, item2));
@@ -71,29 +67,25 @@ public class ItemServiceTest {
 	}
 
 	@Test
-	@DisplayName("登録処理を呼び出したときにmapperの登録処理が1回のみ実行され正常に処理が完了すること")
-	public void testInsertOne() {
+	public void 登録処理を呼び出したときにmapperの登録処理が1回のみ実行され正常に処理が完了すること() {
 		itemService.insertOne(item);
 		verify(itemMapper, times(1)).insertOne(item);
 	}
 
 	@Test
-	@DisplayName("idが2の更新処理を呼び出したときにmapperの更新処理が1回のみ実行され正常に処理が完了すること")
-	public void updateOne() {
+	public void idが2の更新処理を呼び出したときにmapperの更新処理が1回のみ実行され正常に処理が完了すること() {
 		itemService.updateOne(2, "マーガレットピアス", 2400, "ピアス", 3);
 		verify(itemMapper, times(1)).updateOne(2, "マーガレットピアス", 2400, "ピアス", 3);
 	}
 
 	@Test
-	@DisplayName("idが1の削除処理を呼び出したら削除処理が1回のみ実行され正常に処理が完了すること")
-	public void testDeleteOne() {
+	public void idが1の削除処理を呼び出したら削除処理が1回のみ実行され正常に処理が完了すること() {
 		itemService.deleteOne(1);
 		verify(itemMapper, times(1)).deleteOne(1);
 	}
 
 	@Test
-	@DisplayName("initRadioCategoryのMapの値が保持できていること")
-	public void initRadioCategory() {
+	public void initRadioCategoryのMapの値が保持できていること() {
 		Map<String, String> expected = new HashMap<>();
 		expected.put("0", "ピアス");
 		expected.put("1", "イヤリング");
