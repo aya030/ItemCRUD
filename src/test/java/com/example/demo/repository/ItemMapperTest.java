092f60c657f9bbba27065842f157bcfeccb54704
ayn030 <kojinco18.033@gmail.com>
2022-06-27 15:42:10 +0900

    items.ymlを使ったテストへRepositoryテストを修正

--- src/test/java/com/example/demo/repository/ItemMapperTest.java
+++ src/test/java/com/example/demo/repository/ItemMapperTest.java
@@ -8,8 +8,13 @@ import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 @SpringBootTest
 @DBRider
@@ -22,6 +27,7 @@ public class ItemMapperTest {
 	@Test
 	@DataSet("items.yml")
 	public void 検索を1件して結果がIDに紐づく1件だけ取得出来る事() {
+		
 		Item actual = itemMapper.findById(1).get();
 
 		assertEquals("ひまわりピアス", actual.getName());
@@ -30,80 +36,77 @@ public class ItemMapperTest {
 		assertEquals(3, actual.getNum());
 	}
 
-//	@Test
-//	@Order(1)
-//	public void 検索を1件して結果がIDに紐づく1件だけ取得出来る事() throws Exception {
-//		Item findById = itemMapper.findById(1).get();
-//
-//		assertEquals("ひまわりピアス", findById.getName());
-//		assertEquals(3600, findById.getPrice());
-//		assertEquals("ピアス", findById.getCategory());
-//		assertEquals(3, findById.getNum());
-//	}
-
-//	@Test
-//	@Order(2)
-//	public void 検索_全件して結果をリストで取得出来る事() throws Exception {
-//		
-//		assertEquals(3, itemMapper.findAll().size());
-//	}
+	@Test
+	@DataSet("items.yml")
+	public void 検索_全件して結果をリストで取得出来る事() throws Exception {
+		
+		List<Item> actual = itemMapper.findAll();
+		assertEquals(3, actual.size());
+		
+	}
 //	
-//	@Test
-//	@Order(3)
-//	public void 新規登録が出来る事() throws Exception {
-//
-//		Item item = new Item(4, "パンジーイヤリング",2400,"イヤリング",2);
-//		itemMapper.insertOne(item);
-//		
-//		Item findById = itemMapper.findById(4).get();
-//		assertEquals(4, findById.getId());
-//		assertEquals("パンジーイヤリング", findById.getName());
-//		assertEquals(2400, findById.getPrice());
-//		assertEquals("イヤリング", findById.getCategory());
-//		assertEquals(2, findById.getNum());
-//		
-//		assertEquals(4, itemMapper.findAll().size());
-//	}
-//	
-//	@Test
-//	@Order(4)
-//	public void IDに紐づく1件の更新が出来る事() throws Exception {
-//
-//		itemMapper.updateOne(1,"ひまわりイヤリング",3800,"イヤリング",1);
-//		
-//		Item findById = itemMapper.findById(1).get();
-//		assertEquals(1, findById.getId());
-//		assertEquals("ひまわりイヤリング", findById.getName());
-//		assertEquals(3800, findById.getPrice());
-//		assertEquals("イヤリング", findById.getCategory());
-//		assertEquals(1, findById.getNum());
-//	}
-//	
-//	@Test
-//	@Order(5)
-//	public void IDに紐づく1件の削除が出来る事() throws Exception {
-//		
-//		itemMapper.deleteOne(2);
-//		assertEquals(3, itemMapper.findAll().size());
-//	}
-//	
-//	@Test
-//	@Order(6)
-//	public void 既に存在するキーで登録しようとするとDuplicateKeyExceptionとなる事() throws Exception {
-//		
-//		Item item = new Item(1, "チューリップイヤリング",2800,"イヤリング",5);
-//		
-//		assertThatThrownBy(() -> {
-//			itemMapper.insertOne(item);
-//		}).isInstanceOf(DuplicateKeyException.class);
-//	}
-//	
-//	@Test
-//	@Order(7)
-//	public void NULLで登録しようとするとDataIntegrityViolationExceptionとなる事() throws Exception {
-//		
-//		assertThatThrownBy(() -> {
-//			itemMapper.insertOne(null);
-//		}).isInstanceOf(DataIntegrityViolationException.class);
-//	}	
+	@Test
+	@DataSet("items.yml")
+	public void 新規登録が出来る事() throws Exception {
+		
+		Item item = new Item(4, "パンジーイヤリング",2400,"イヤリング",2);
+		itemMapper.insertOne(item);
+		Item actual = itemMapper.findById(4).get();
+		
+		/* 新規登録されたIDに紐づいた値が同じかどうか */
+		assertEquals(4, actual.getId());
+		assertEquals("パンジーイヤリング", actual.getName());
+		assertEquals(2400, actual.getPrice());
+		assertEquals("イヤリング", actual.getCategory());
+		assertEquals(2, actual.getNum());
+		
+		/* 新規登録されてリストの数が増えているかどうか */
+		assertEquals(4, itemMapper.findAll().size());
+	}
+	
+	@Test
+	@DataSet("items.yml")
+	public void IDに紐づく1件の更新が出来る事() throws Exception {
+		
+		itemMapper.updateOne(1,"チューリップイヤリング",3800,"イヤリング",1);
+		Item actual = itemMapper.findById(1).get();
+		
+		assertEquals(1, actual.getId());
+		assertEquals("チューリップイヤリング", actual.getName());
+		assertEquals(3800, actual.getPrice());
+		assertEquals("イヤリング", actual.getCategory());
+		assertEquals(1, actual.getNum());
+		
+	}
+	
+	@Test
+	@DataSet("items.yml")
+	public void IDに紐づく1件の削除が出来る事() throws Exception {
+		
+		itemMapper.deleteOne(2);
+		
+		List<Item> actual = itemMapper.findAll();
+		assertEquals(2, actual.size());
+	}
+	
+	@Test
+	@DataSet("items.yml")
+	public void 既に存在するキーで登録しようとするとDuplicateKeyExceptionとなる事() throws Exception {
+		
+		Item item = new Item(1, "チューリップイヤリング",2800,"イヤリング",5);
+		
+		assertThatThrownBy(() -> {
+			itemMapper.insertOne(item);
+		}).isInstanceOf(DuplicateKeyException.class);
+	}
+	
+	@Test
+	public void NULLで登録しようとするとDataIntegrityViolationExceptionとなる事() throws Exception {
+		
+		assertThatThrownBy(() -> {
+			itemMapper.insertOne(null);
+		}).isInstanceOf(DataIntegrityViolationException.class);
+	}
+	
 }
+
